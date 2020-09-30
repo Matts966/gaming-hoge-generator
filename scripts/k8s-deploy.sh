c10d5e616a629a19825d968e2d0a6bf57e0264f6
Matts966 <28551465+Matts966@users.noreply.github.com>
2020-10-01 08:10:42 +0900

    Update k8s-deploy.sh

--- scripts/k8s-deploy.sh
+++ scripts/k8s-deploy.sh
@@ -1,6 +1,6 @@
 MANIFESTS_JSON=$(curl https://api.github.com/repos/Matts966/gaming-hoge-generator/contents/manifests)
-NUM=$(echo $json | jq length)
+NUM=$(echo $MANIFESTS_JSON | jq length)
 
 for i in $( seq 0 $(($NUM - 1)) ); do
-  sudo kubectl apply -f $(echo $json | jq .[$i].download_url)
+  sudo kubectl apply -f $(echo $MANIFESTS_JSON | jq .[$i].download_url)
 done
